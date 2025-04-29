97687be995f97170c9126d4cb6d1544a0bf3934e
Ayoub Mentag <amentag@e2r3p12.1337.ma>
2025-04-29 18:17:55 +0100

    Finish p2

---
+++ p2/scripts/install_k3s_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+START=$(date +%s)
+
+# Install K3s in controller mode
+curl -sfL https://get.k3s.io | sh -
+
+# Save the token to a shared directory so the worker can access it
+mkdir -p /vagrant/shared
+cp /var/lib/rancher/k3s/server/node-token /vagrant/shared/token
+ln -sf /usr/local/bin/kubectl /usr/bin/kubectl
+chown vagrant /etc/rancher/k3s/k3s.yaml
+apt update && apt install net-tools
+
+# kubectl apply -f /vagrant/k3s-configs/pp1.yaml
+# kubectl apply -f /vagrant/k3s-configs/app2.yaml
+# kubectl apply -f /vagrant/k3s-configs/app3.yaml
+# kubectl apply -f /vagrant/k3s-configs/ingress.yaml
+
+
+END=$(date +%s)
+echo "⏱️ Provisioning script ran for $(($END - $START)) seconds"
