d5d32f103e217e05592fa060ca426acdca5a99f2
Ayoub Mentag <amentag@e2r3p12.1337.ma>
2025-04-29 19:39:40 +0100

    Finish p2

--- p2/scripts/install_k3s_server.sh
+++ p2/scripts/install_k3s_server.sh
@@ -2,6 +2,14 @@
 
 START=$(date +%s)
 
+# install some necessary packages
+apt update
+apt install net-tools
+apt install zsh -y
+export RUNZSH=no
+export CHSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install K3s in controller mode
 curl -sfL https://get.k3s.io | sh -
 
@@ -10,12 +18,7 @@ mkdir -p /vagrant/shared
 cp /var/lib/rancher/k3s/server/node-token /vagrant/shared/token
 ln -sf /usr/local/bin/kubectl /usr/bin/kubectl
 chown vagrant /etc/rancher/k3s/k3s.yaml
-apt update && apt install net-tools
-
-# kubectl apply -f /vagrant/k3s-configs/pp1.yaml
-# kubectl apply -f /vagrant/k3s-configs/app2.yaml
-# kubectl apply -f /vagrant/k3s-configs/app3.yaml
-# kubectl apply -f /vagrant/k3s-configs/ingress.yaml
+kubectl apply -f /vagrant/configs --recursive
 
 
 END=$(date +%s)
