a4748bf1d0a8d17bd3284d78a54e25a71c02da57
Ayoub Mentag <amentag@e3r4p1.1337.ma>
2025-05-30 16:31:42 +0100

    Second part finished

--- p2/scripts/install_k3s_server.sh
+++ p2/scripts/install_k3s_server.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-START=$(date +%s)
-
-# install some necessary packages
 apt update
 apt install net-tools
 
-# Install K3s in controller mode
 curl -sfL https://get.k3s.io | sh -
 chown vagrant /etc/rancher/k3s/k3s.yaml
-# apply k3s configs
 kubectl apply -f /vagrant/configs --recursive
-
-END=$(date +%s)
-echo "⏱️ Provisioning script ran for $(($END - $START)) seconds"
