f8fda62727255e22e2d003ef73009f580cee3c63
Ayoub Mentag <amentag@e3r4p1.1337.ma>
2025-05-30 17:59:09 +0100

    Third part finished

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Create cluster
-k3d cluster delete my-cluster || true
-k3d cluster create my-cluster
+k3d cluster delete my-cluster
+k3d cluster create my-cluster -p "9000:8888@loadbalancer"
 
-# Create namespaces
-kubectl create namespace argocd || true
-kubectl create namespace dev || true
+kubectl create namespace argocd
+kubectl create namespace dev
 
-# Install Argo CD
-echo "[INFO] Installing Argo CD into Kubernetes..."
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Waiting for Argo CD server to be ready..."
 kubectl wait --for=condition=available --timeout=180s -n argocd deploy/argocd-server
 
-# Apply Argo CD Application resource (points to your GitHub repo)
-kubectl apply -f ./confs/argocd-app.yaml
+
+# add makefile and change this path 
+kubectl apply -f ../confs/argocd-app.yaml
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
